2128b0a4b9738b8931cda5cbe3b78c50f6496b2b
Henrique Jardim <contato@agenciacodigo.com>
2026-01-19 16:38:45 -0300

    Fix: Forcando Django a servir media files em producao

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     # 1. Painel Admin (Unfold)
@@ -9,9 +10,10 @@ urlpatterns = [
 
     # 2. Rotas da API do Site
     path("api/", include("core.urls")), 
+
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# Servir mídia em desenvolvimento
+# Servir estáticos em desenvolvimento (Opcional, mas bom manter)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
